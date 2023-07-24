e09f2f9c264edb89139088f89e429a2ca67ade05
ibrahimsheikhoon <i.sheikhon@tweeq.sa>
2023-07-24 12:05:54 +0300

    separate database function from services and restructor project folder

---
+++ api/transactions/database/db.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/google/uuid"
+	_ "github.com/google/uuid"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"time"
+	"transaction/server/proto/transaction"
+)
+
+var db, err = sql.Open("postgres", "postgresql://root@localhost:26257/defaultdb?sslmode=disable")
+
+func DBGetTransactionByID(req *transaction.TransactionQueryRequest) (*transaction.TransactionResponse, error) {
+	trans := transaction.Transaction{}
+	var created time.Time
+	query := "SELECT * FROM transaction where id = $1"
+	err := db.QueryRow(query, req.ID).Scan(&trans.ID, &trans.Name, &trans.TotalPrice,
+		&trans.ProductID, &trans.CustomerID, &trans.Quantity, &created)
+	if err != nil {
+		panic(err)
+	}
+	return &transaction.TransactionResponse{Transaction: &transaction.Transaction{ID: trans.ID, Name: trans.Name, TotalPrice: trans.TotalPrice, ProductID: trans.ProductID, CustomerID: trans.CustomerID, Quantity: trans.Quantity, CreatedAt: timestamppb.New(created)}}, nil
+}
+
+func DBAddTransaction(req *transaction.Transaction) (*transaction.TransactionResponse, error) {
+	log.Printf("Inserting a Row in to DB")
+	var UUID = uuid.New().String()
+	//Inserting a Row in to DB.
+	sqlStatement := `INSERT INTO transaction (id,name,totalprice,productid,customerid,quantity,created_at) VALUES ($1, $2, $3, $4,$5,$6,$7)`
+	_, err = db.Exec(sqlStatement, UUID, req.Name, req.TotalPrice*req.Quantity, req.ProductID, req.CustomerID, req.Quantity, time.Now())
+	if err != nil {
+		panic(err)
+	}
+	return &transaction.TransactionResponse{Transaction: &transaction.Transaction{ID: UUID, Name: req.Name, TotalPrice: req.TotalPrice * req.Quantity, ProductID: req.ProductID, CustomerID: req.CustomerID, Quantity: req.Quantity, CreatedAt: req.CreatedAt}}, nil
+
+}
+
+func DBGetAllTransaction(req *transaction.AllTransactionQueryRequest) (*transaction.TransactionListResponse, error) {
+	var trans []*transaction.Transaction
+	var created time.Time
+	query := "SELECT * FROM transaction"
+	rows, err := db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		tra := transaction.Transaction{}
+		rows.Scan(&tra.ID, &tra.Name, &tra.TotalPrice,
+			&tra.ProductID, &tra.CustomerID, &tra.Quantity, &created)
+		trans = append(trans, &transaction.Transaction{ID: tra.ID, Name: tra.Name, TotalPrice: tra.TotalPrice, ProductID: tra.ProductID, CustomerID: tra.CustomerID, Quantity: tra.Quantity, CreatedAt: timestamppb.New(created)})
+	}
+	log.Printf("Received request: %v", req.ProtoReflect().Descriptor().FullName())
+	return &transaction.TransactionListResponse{Transactions: trans}, nil
+}
---
+++ api/transactions/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"transaction/server/proto/transaction"
+	database_ "transaction/server/transactions/database"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	transaction.UnimplementedTransactionServiceServer
+}
+
+func (s *server) AddTransaction(ctx context.Context, req *transaction.Transaction) (*transaction.TransactionResponse, error) {
+	log.Printf("Received request: %v", req.ProtoReflect().Descriptor().FullName())
+	return database_.DBAddTransaction(req)
+}
+
+func (s *server) GetAllTransaction(ctx context.Context, req *transaction.AllTransactionQueryRequest) (*transaction.TransactionListResponse, error) {
+	return database_.DBGetAllTransaction(req)
+}
+func (s *server) FindTransactionByID(ctx context.Context, req *transaction.TransactionQueryRequest) (*transaction.TransactionResponse, error) {
+	log.Printf("Received request: %v", req.ProtoReflect().Descriptor().FullName())
+	return database_.DBGetTransactionByID(req)
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	transaction.RegisterTransactionServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
