4432bd157623f8910c5144d5ce1e1d9f90f00ec2
ibrahimsheikhoon <i.sheikhon@tweeq.sa>
2023-07-24 13:37:41 +0300

    add customer rpcs and db

---
+++ api/customers/database/db.go
@@ -0,0 +1,48 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/google/uuid"
+	"log"
+	"transaction/server/proto/customer"
+)
+
+var db, err = sql.Open("postgres", "postgresql://root@localhost:26258/defaultdb?sslmode=disable")
+
+func DBGetCustomerByID(req *customer.CustomerQueryRequest) (*customer.CustomerResponse, error) {
+	cr := customer.Customer{}
+	query := "SELECT * FROM customer where id = $1"
+	err := db.QueryRow(query, req.ID).Scan(&cr.ID, &cr.Name)
+	if err != nil {
+		panic(err)
+	}
+	return &customer.CustomerResponse{Customer: &customer.Customer{ID: cr.ID, Name: cr.Name}}, nil
+}
+
+func DBAddCustomer(req *customer.Customer) (*customer.CustomerResponse, error) {
+	log.Printf("Inserting a Row in to DB")
+	var UUID = uuid.New().String()
+	//Inserting a Row in to DB.
+	sqlStatement := `INSERT INTO customer (id,name) VALUES ($1, $2)`
+	_, err = db.Exec(sqlStatement, UUID, req.Name)
+	if err != nil {
+		panic(err)
+	}
+	return &customer.CustomerResponse{Customer: &customer.Customer{ID: UUID, Name: req.Name}}, nil
+}
+
+func DBGetAllCustomer(req *customer.AllCustomersQueryRequest) (*customer.CustomerListResponse, error) {
+	var customers []*customer.Customer
+	query := "SELECT * FROM customer"
+	rows, err := db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		cr := customer.Customer{}
+		rows.Scan(&cr.ID, &cr.Name)
+		customers = append(customers, &customer.Customer{ID: cr.ID, Name: cr.Name})
+	}
+	log.Printf("Received request: %v", req.ProtoReflect().Descriptor().FullName())
+	return &customer.CustomerListResponse{Customers: customers}, nil
+}
---
+++ api/customers/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	database_ "transaction/server/customers/database"
+	"transaction/server/proto/customer"
+)
+
+var (
+	port = flag.Int("port", 50052, "The server port")
+)
+
+type server struct {
+	customer.UnimplementedCustomerServiceServer
+}
+
+func (s *server) AddCustomer(ctx context.Context, req *customer.Customer) (*customer.CustomerResponse, error) {
+	log.Printf("Received request: %v", req.ProtoReflect().Descriptor().FullName())
+	return database_.DBAddCustomer(req)
+}
+func (s *server) GetAllCustomers(ctx context.Context, req *customer.AllCustomersQueryRequest) (*customer.CustomerListResponse, error) {
+	log.Printf("Received request: %v", req.ProtoReflect().Descriptor().FullName())
+	return database_.DBGetAllCustomer(req)
+}
+
+func (s *server) FindCustomerByID(ctx context.Context, req *customer.CustomerQueryRequest) (*customer.CustomerResponse, error) {
+	log.Printf("Received request: %v", req.ProtoReflect().Descriptor().FullName())
+	return database_.DBGetCustomerByID(req)
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	customer.RegisterCustomerServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
